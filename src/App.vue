35019d22efb57e86c383ea5b15e202997dd485b1
Kurt Puican Salas <u202016643@upc.edu.pe>
2023-04-05 11:22:49 -0500

    repositorio creado kurt

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Headerproject from './components/Headerproject.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+   <Headerproject/>
+   <Headerproject name="componente 1"/>
+   <Headerproject name="componente 2"/>
+   <Headerproject name="componente 3"/>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Headerproject.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>My component {{name}}</div>
+
+</template>
+
+<script>
+
+export default {
+    props:['name'],
+    name: "header"
+}
+</script>
+
+<style scoped>
+
+</style>
